e87257a95d58c5a8c893941ff52645a97c622267
Eiichiro Uchiumi <eiichiro.uchiumi@gmail.com>
2012-06-10 02:21:58 +0900

    Updated the version of Jaguar to 2.0-rc3

--- src/test/java/org/eiichiro/gig/Scope1Context.java
+++ src/test/java/org/eiichiro/gig/Scope1Context.java
@@ -1,16 +1,16 @@
 package org.eiichiro.gig;
 
-import org.eiichiro.jaguar.Description;
+import org.eiichiro.jaguar.Descriptor;
 import org.eiichiro.jaguar.scope.Context;
 
 public class Scope1Context implements Context {
 
 	@Override
-	public <T> T get(Description<T> description) {
+	public <T> T get(Descriptor<T> descriptor) {
 		return null;
 	}
 
 	@Override
-	public <T> void put(Description<T> description, T t) {}
+	public <T> void put(Descriptor<T> descriptor, T t) {}
 
 }
